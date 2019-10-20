a66b5a214314a537722605521dca1caaa576f8d6
Mommel <mommel@gmx.de>
2019-10-20 11:03:28 +0200

    update electron

--- app/reducers/index.js
+++ app/reducers/index.js
@@ -3,7 +3,7 @@ import { combineReducers } from 'redux';
 import { connectRouter } from 'connected-react-router';
 
 export default function createRootReducer(history: History) {
-  return combineReducers({
+  return combineReducers<{}, *>({
     router: connectRouter(history)
   });
 }
--- configs/webpack.config.base.js
+++ configs/webpack.config.base.js
@@ -26,6 +26,7 @@ export default {
 
   output: {
     path: path.join(__dirname, '..', 'app'),
+    // https://github.com/webpack/webpack/issues/1114
     libraryTarget: 'commonjs2'
   },
 
